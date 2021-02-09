a19250c8235f93a896f32a263036bfdf2d72085e
Ryoto-kubo <sax.guiter@icloud.com>
2021-02-09 23:30:15 +0900

    change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,6 @@
-import Link from "next/link";
-import styled from "styled-components";
 import Layout from "../components/Layout";
-const Heading1 = styled.h1`
-  font-size: 1.2rem;
-  color: red;
-`;
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <Heading1 className="title">Hello Next.js 👋</Heading1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <Layout title="KanonCode | コードレビュを全てのエンジニアへ"></Layout>
 );
 
 export default IndexPage;
