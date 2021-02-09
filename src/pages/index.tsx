3971f34956cfa4d88c5c51e61d612573ee6e2214
Ryoto-kubo <sax.guiter@icloud.com>
2021-02-10 00:25:43 +0900

    import reset.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/layouts";
 import styled from "styled-components";
+import 'modern-css-reset/dist/reset.min.css'
 const Heading1 = styled.h1`
   font-size: 1.2rem;
   color: red;
