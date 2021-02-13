e68cd618d406db05f7e80f92c4cce41a0d8e7841
Ryoto-kubo <sax.guiter@icloud.com>
2021-02-13 21:06:41 +0900

    change color

--- src/components/atoms/Typography.tsx
+++ src/components/atoms/Typography.tsx
@@ -13,12 +13,7 @@ export const CustomTypography: React.FC<Props> = (props) => {
   const StyledParagraph = styled(Typography)`
     font-size: ${props.fontSize}px;
     font-weight: ${props.fontWeight};
-    color: ${props.isPrimary ? theme.palette.primary.main : "#020202"};
-    margin-bottom: 0px;
+    color: ${props.isPrimary ? theme.palette.primary.main : "#5C6BC0"};
   `;
-  return (
-    <StyledParagraph variant="body1" gutterBottom>
-      {props.children}
-    </StyledParagraph>
-  );
+  return <StyledParagraph variant="body1">{props.children}</StyledParagraph>;
 };
