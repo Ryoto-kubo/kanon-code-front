91852b2c2a9fb9eadf7d55021f7a294d5ad03f84
Ryoto-kubo <sax.guiter@icloud.com>
2021-02-22 00:08:21 +0900

    Change the location of the css

--- src/components/organisms/SignInContent.tsx
+++ src/components/organisms/SignInContent.tsx
@@ -2,21 +2,20 @@ import { SignInButtons } from '@/components/molecules/SignInButtons'
 import { SignInTexts } from '@/components/molecules/SignInTexts'
 import { Box } from '@material-ui/core/'
 import React from 'react'
-import styled from 'styled-components'
 
-const StyledBox = styled(Box)`
-  width: 100%;
-  text-align: center;
-  position: relative;
-  top: 300px;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`
+// const StyledBox = styled(Box)`
+//   width: 100%;
+//   text-align: center;
+//   position: relative;
+//   top: 300px;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+// `
 export const SignInContent: React.FC = () => {
   return (
-    <StyledBox>
+    <Box>
       <SignInTexts />
       <SignInButtons />
-    </StyledBox>
+    </Box>
   )
 }
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -2,12 +2,21 @@ import { SignInContent } from '@/components/organisms/SignInContent'
 import { LayoutNoFooter } from '@/layouts/no-footer'
 import { Container } from '@material-ui/core/'
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledContainer = styled(Container)`
+  width: 100%;
+  text-align: center;
+  position: relative;
+  top: 300px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
 const IndexPage: React.FC = () => (
   <LayoutNoFooter title="Kanon Code | サインイン">
-    <Container>
+    <StyledContainer>
       <SignInContent />
-    </Container>
+    </StyledContainer>
   </LayoutNoFooter>
 )
 export default IndexPage
