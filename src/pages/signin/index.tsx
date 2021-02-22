4d8f5516185d76b0beeec94af6d85db977184652
Ryoto-kubo <sax.guiter@icloud.com>
2021-02-22 23:03:13 +0900

    change layout

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,5 +1,6 @@
 import { SignInContent } from '@/components/organisms/SignInContent'
-import { LayoutNoFooter } from '@/layouts/no-footer'
+// import { LayoutNoFooter } from '@/layouts/no-footer'
+import Layout from '@/layouts/standard'
 import { Container } from '@material-ui/core/'
 import React from 'react'
 import styled from 'styled-components'
@@ -7,17 +8,18 @@ import styled from 'styled-components'
 const StyledContainer = styled(Container)`
   width: 100%;
   text-align: center;
-  // margin-top: 40px;
+  margin-top: 40px;
+  margin-bottom: 40px;
   // position: relative;
   // top: 250px;
   // left: 50%;
   // transform: translate(-50%, -50%);
 `
 const IndexPage: React.FC = () => (
-  <LayoutNoFooter title="Kanon Code | サインイン">
+  <Layout title="Kanon Code | サインイン">
     <StyledContainer>
       <SignInContent />
     </StyledContainer>
-  </LayoutNoFooter>
+  </Layout>
 )
 export default IndexPage
