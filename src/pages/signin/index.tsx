c92566141f59d0f2c73df88c9272b82cb22ca99c
Ryoto-kubo <sax.guiter@icloud.com>
2021-02-23 17:23:39 +0900

    remove not used cord

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,21 +1,21 @@
 import { SignInContent } from '@/components/organisms/SignInContent'
 import { LayoutNoFooter } from '@/layouts/no-footer'
 // import Layout from '@/layouts/standard'
-import { Container } from '@material-ui/core/'
+// import { Container } from '@material-ui/core/'
 import React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-const StyledContainer = styled(Container)`
-  width: 100%;
-  text-align: center;
-  margin-top: 70px;
-  margin-bottom: 40px;
-`
+// const StyledContainer = styled(Container)`
+//   width: 100%;
+//   text-align: center;
+//   margin-top: 70px;
+//   margin-bottom: 40px;
+// `
 const IndexPage: React.FC = () => (
   <LayoutNoFooter title="Kanon Code | サインイン">
-    <StyledContainer>
-      <SignInContent />
-    </StyledContainer>
+    {/* <StyledContainer> */}
+    <SignInContent />
+    {/* </StyledContainer> */}
   </LayoutNoFooter>
 )
 export default IndexPage
