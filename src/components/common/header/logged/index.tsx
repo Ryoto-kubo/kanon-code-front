fc1682483c000f39f3207a6e5d512d468fb5e906
Ryoto-kubo <sax.guiter@icloud.com>
2021-03-05 00:02:35 +0900

    created header for after signin

--- src/components/common/header/logged/index.tsx
+++ src/components/common/header/logged/index.tsx
@@ -1,9 +1,9 @@
 import { KanonCodeLogo } from "@/components/atoms/Logo";
 import { SolidLink } from "@/components/atoms/SolidLink";
 import { CustomStickyAppBar } from "@/components/atoms/StickyAppBar";
-// import { ICognitoUserData } from "@aws-amplify/auth";
-// import {CognitoUserSession} from 'amazon-cognito-identity-js'
-import { Box } from "@material-ui/core/";
+import { Box, Input } from "@material-ui/core/";
+import IconButton from "@material-ui/core/IconButton";
+import { Notifications, Search } from "@material-ui/icons";
 import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
@@ -20,7 +20,7 @@ const StyledBox = styled(Box)`
 `;
 const StyledUseMr = styled.span`
   &:not(:last-child) {
-    margin-right: 16px;
+    margin-right: 24px;
   }
 `;
 const StyledImg = styled.img`
@@ -28,10 +28,23 @@ const StyledImg = styled.img`
   width: 40px;
   width: 40px;
 `;
+const StyledIconButton = styled(IconButton)`
+  padding: 0;
+  &:hover {
+    background: none;
+  }
+`;
+const StyledInput = styled(Input)`
+  font-size: 14px;
+`;
 
 export const TheLoggedHeader: React.FC<Props> = (props) => {
   console.log(props.authUser);
   const userInfo = props.authUser.signInUserSession.idToken.payload;
+  const func = (e: Event) => {
+    console.log("search");
+    e.preventDefault();
+  };
 
   return (
     <CustomStickyAppBar>
@@ -46,6 +59,24 @@ export const TheLoggedHeader: React.FC<Props> = (props) => {
           </a>
         </Link>
         <Box display="flex" alignItems="center">
+          <StyledUseMr>
+            <form onSubmit={func}>
+              <StyledInput
+                placeholder="キーワード検索"
+                inputProps={{ "aria-label": "description" }}
+                endAdornment={
+                  <StyledIconButton disableRipple={true} onClick={func}>
+                    <Search />
+                  </StyledIconButton>
+                }
+              />
+            </form>
+          </StyledUseMr>
+          <StyledUseMr>
+            <StyledIconButton disableRipple={true}>
+              <Notifications />
+            </StyledIconButton>
+          </StyledUseMr>
           <StyledUseMr>
             <SolidLink href="/posts/new">レビューを依頼する</SolidLink>
           </StyledUseMr>
