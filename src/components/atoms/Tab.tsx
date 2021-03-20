79b05a3a5c3e77c9aeb5c7c6a6a294e9721bf2e5
tetsuro.tayama <tetsuro.tayama@gmail.com>
2021-03-20 14:31:14 +0900

    Update Tab.tsx

--- src/components/atoms/Tab.tsx
+++ src/components/atoms/Tab.tsx
@@ -7,35 +7,21 @@ type Props = {
   label: string
   href: string
   value: string
-  onChange?: (event: React.ChangeEvent<{ checked: boolean }>, value: any) => void;
-  onClick?: React.EventHandler<any>;        
-}
-type LinkTabProps = {
-  label?: string
-  href?: string
-  value: string
 }
 
-const StyledTab = styled(LinkTab)`
+const StyledTab = styled(Tab)`
   min-width: 130px;
   font-size: 16px;
 `
 
-function LinkTab(props: LinkTabProps) {
-  return (
-  <Link href={props.href}>
-    <Tab
-      // onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      //   event.preventDefault()
-      // }}
-
-      disableRipple={true}
-      {...props}
-    />
-    </Link>
-  )
-}
-
 export const CustomTab: React.FC<Props> = (props) => {
-  return <StyledTab label={props.label} value={props.value} href={props.href} />
-}
+    return (
+   <Link href={props.href}>   
+    <StyledTab
+    disableRipple={true}
+     {...props}
+  />
+    </Link> 
+    ) 
+  }
+  
