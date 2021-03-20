dbbbbc7619163bbb982f5e0b8680574ec4412ea2
Ryoto-kubo <sax.guiter@icloud.com>
2021-03-20 12:34:10 +0900

    recreate components

--- src/components/organisms/TabsHeader.tsx
+++ src/components/organisms/TabsHeader.tsx
@@ -1,80 +1,63 @@
-import { Heading2 } from '@/components/atoms/Heading2'
-import { Tabs } from '@/components/molecules/Tabs'
-// import Tab from '@material-ui/core/Tab'
-// import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Tabs from '@material-ui/core/Tabs'
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = {
-  text: string
   value: string | number
+  tabs: Array<{ label: string; value: string; href: string }>
   handleChange: (event: React.ChangeEvent<{}>, newValue: string) => void
 }
-// type LinkTabProps = {
-//   label?: string
-//   href?: string
-//   value: string
-// }
+type LinkTabProps = {
+  label?: string
+  href?: string
+  value: string
+}
 
-// const StyledTabs = styled(Tabs)`
-//   border-bottom: 1px solid #e8e8e8;
-// `
-// const StyledTab = styled(LinkTab)`
-//   min-width: 130px;
-//   font-size: 16px;
-//   // font-weight: bold;
-// `
-// function LinkTab(props: LinkTabProps) {
-//   return (
-//     <Tab
-//       component="a"
-//       onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-//         event.preventDefault()
-//       }}
-//       disableRipple={true}
-//       {...props}
-//     />
-//   )
-// }
+const StyledTabs = styled(Tabs)`
+  border-bottom: 1px solid #e8e8e8;
+`
+const StyledTab = styled(Tab)`
+  min-width: 130px;
+  font-size: 16px;
+`
+
+const LinkTab = (props: LinkTabProps) => {
+  return (
+    <StyledTab
+      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault()
+      }}
+      disableRipple={true}
+      {...props}
+    />
+  )
+}
 
 export const TabsHeader: React.FC<Props> = (props) => {
+  const renderTab = () => {
+    const results = []
+    for (const [index, item] of props.tabs.entries()) {
+      results.push(
+        <LinkTab
+          key={index}
+          label={item.label}
+          value={item.value}
+          href={item.href}
+        />,
+      )
+    }
+    return results
+  }
+
   return (
-    <>
-      <Heading2 fontSize={24} marginBottom={1}>
-        {props.text}
-      </Heading2>
-      <Tabs value={props.value} handleChange={props.handleChange} />
-      {/* <StyledTabs
-        value={props.value}
-        onChange={props.handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-      >
-        <StyledTab
-          label="プロフィール"
-          value="/settings/profile"
-          href="/settings/profile"
-        />
-        <StyledTab
-          label="アカウント"
-          value="/settings/account"
-          href="/settings/account"
-        />
-        <StyledTab
-          label="カード情報"
-          value="/settings/billing"
-          href="/settings/billing"
-        />
-        <StyledTab
-          label="購入履歴"
-          value="/settings/payments-history"
-          href="/settings/payments-history"
-        />
-        <StyledTab
-          label="お振込先"
-          value="/settings/bank"
-          href="/settings/bank"
-        />
-      </StyledTabs> */}
-    </>
+    <StyledTabs
+      value={props.value}
+      onChange={props.handleChange}
+      indicatorColor="primary"
+      textColor="primary"
+    >
+      {renderTab()}
+    </StyledTabs>
   )
 }
--- src/layouts/setting/index.tsx
+++ src/layouts/setting/index.tsx
@@ -1,45 +1,20 @@
-// import { Heading2 } from '@/components/atoms/Heading2'
+import { Heading2 } from '@/components/atoms/Heading2'
 import { TheFooter } from '@/components/common/footer/index'
 import { CommonHead } from '@/components/common/head/index'
 import { TheLoggedHeader } from '@/components/common/header/logged'
 import { TheStndardHeader } from '@/components/common/header/standard'
 import { TabsHeader } from '@/components/organisms/TabsHeader'
+import { settingTabs } from '@/consts/setting-tabs'
 import { Toolbar } from '@material-ui/core'
 import { Container } from '@material-ui/core/'
-import Tab from '@material-ui/core/Tab'
-import Tabs from '@material-ui/core/Tabs'
 import { useRouter } from 'next/router'
 import React, { ReactNode, useState } from 'react'
 import styled from 'styled-components'
-// ;<a
-//   class="MuiButtonBase-root MuiTab-root MuiTab-textColorPrimary setting__StyledTab-sc-1kjsxfi-3 jYFBnq"
-//   tabindex="-1"
-//   aria-disabled="false"
-//   role="tab"
-//   aria-selected="false"
-//   href="/settings/profile"
-// >
-//   <span class="MuiTab-wrapper">プロフィール</span>
-// </a>
-// ;<a
-//   class="MuiButtonBase-root MuiTab-root MuiTab-textColorInherit Tab__StyledTab-sc-1tm8ywy-0 cszAjb"
-//   tabindex="-1"
-//   aria-disabled="false"
-//   role="tab"
-//   href="/settings/profile"
-// >
-//   <span class="MuiTab-wrapper">プロフィール</span>
-// </a>
 
 type Props = {
   children: ReactNode
   title: string
   authUser: any
-}
-type LinkTabProps = {
-  label?: string
-  href?: string
-  value: string
 }
 
 const StyledMain = styled.main`
@@ -49,26 +24,6 @@ const StyledContainer = styled(Container)`
   width: 100%;
   margin-top: 70px;
 `
-const StyledTabs = styled(Tabs)`
-  border-bottom: 1px solid #e8e8e8;
-`
-const StyledTab = styled(LinkTab)`
-  min-width: 130px;
-  font-size: 16px;
-  // font-weight: bold;
-`
-function LinkTab(props: LinkTabProps) {
-  return (
-    <Tab
-      component="a"
-      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        event.preventDefault()
-      }}
-      disableRipple={true}
-      {...props}
-    />
-  )
-}
 
 export const SettingLayout = ({
   children,
@@ -92,48 +47,14 @@ export const SettingLayout = ({
       <Toolbar />
       <StyledContainer maxWidth="md">
         <StyledMain>
+          <Heading2 fontSize={24} marginBottom={1}>
+            アカウント設定
+          </Heading2>
           <TabsHeader
             value={value}
             handleChange={handleChange}
-            text="アカウント設定"
+            tabs={settingTabs}
           />
-
-          {/* <Heading2 fontSize={24} marginBottom={1}>
-            アカウント設定
-          </Heading2>
-          <StyledTabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-          >
-            <StyledTab
-              label="プロフィール"
-              value="/settings/profile"
-              href="/settings/profile"
-            />
-            <StyledTab
-              label="アカウント"
-              value="/settings/account"
-              href="/settings/account"
-            />
-            <StyledTab
-              label="カード情報"
-              value="/settings/billing"
-              href="/settings/billing"
-            />
-            <StyledTab
-              label="購入履歴"
-              value="/settings/payments-history"
-              href="/settings/payments-history"
-            />
-            <StyledTab
-              label="お振込先"
-              value="/settings/bank"
-              href="/settings/bank"
-            />
-          </StyledTabs> */}
-
           {children}
         </StyledMain>
       </StyledContainer>
