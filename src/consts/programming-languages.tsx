9bb80d26fb4f5de036fd35291db11479c4d93355
Ryoto-kubo <sax.guiter@icloud.com>
2021-03-27 16:07:48 +0900

    create page setting/skils

---
+++ src/consts/programming-languages.tsx
@@ -0,0 +1,30 @@
+export const programmingLanguages = [
+  {
+    value: 'JavaScript',
+    label: 'javascript',
+  },
+  {
+    value: 'Vue.js',
+    label: 'vue.js',
+  },
+  {
+    value: 'React.js',
+    label: 'react.js',
+  },
+  {
+    value: 'Angular',
+    label: 'angular',
+  },
+  {
+    value: 'Node.js',
+    label: 'node.js',
+  },
+  {
+    value: 'PHP',
+    label: 'php',
+  },
+  {
+    value: 'java',
+    label: 'java',
+  },
+]
--- src/consts/select-options.tsx
+++ src/consts/select-options.tsx
@@ -20,3 +20,30 @@ export const positions = [
     label: 'インフラエンジニア',
   },
 ]
+
+export const yearsExperience = [
+  {
+    value: 0,
+    label: '~1年',
+  },
+  {
+    value: 1,
+    label: '1年~2年',
+  },
+  {
+    value: 2,
+    label: '2年~3年',
+  },
+  {
+    value: 3,
+    label: '3年~4年',
+  },
+  {
+    value: 4,
+    label: '4年~5年',
+  },
+  {
+    value: 5,
+    label: '5年~',
+  },
+]
