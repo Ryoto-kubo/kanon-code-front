bc8ad426b5204ccaa504aa320992576a7f2b7365
Ryoto-kubo <sax.guiter@icloud.com>
2021-03-29 12:39:47 +0900

    fix logic

--- src/components/atoms/SocialSignInButton.tsx
+++ src/components/atoms/SocialSignInButton.tsx
@@ -1,27 +1,27 @@
-import { Box, Button } from "@material-ui/core/"
-import React from "react"
-import styled from "styled-components"
+import { Box, Button } from "@material-ui/core/";
+import React from "react";
+import styled from "styled-components";
 
 interface Props {
-  text: string
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+  text: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const StyledButton = styled(Button)`
-  max-height: 36px
-`
+  max-height: 36px;
+`;
 const StyledBox = styled(Box)`
-  margin-right: 16px
-  display: flex
-`
+  margin-right: 16px;
+  display: flex;
+`;
 const StyledSpan = styled.span`
-  font-weight: bold
-`
+  font-weight: bold;
+`;
 export const SocialSignInButton: React.FC<Props> = (props) => {
   return (
     <StyledButton variant="outlined" onClick={props.onClick}>
       <StyledBox>{props.children}</StyledBox>
       <StyledSpan>{props.text}</StyledSpan>
     </StyledButton>
-  )
-}
+  );
+};
--- src/components/molecules/SignInGoogleButtons.tsx
+++ src/components/molecules/SignInGoogleButtons.tsx
@@ -1,17 +1,17 @@
-import { SocialSignInButton } from '@/components/atoms/SocialSignInButton'
-import React from 'react'
-import GoogleLogoSvg from '../../assets/logo/google.svg'
+import { SocialSignInButton } from "@/components/atoms/SocialSignInButton";
+import React from "react";
+import GoogleLogoSvg from "../../assets/logo/google.svg";
 
 interface Props {
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const SignInGoogleButton: React.FC<Props> = (props) => {
   return (
     <>
       <SocialSignInButton text="Sign in with Google" onClick={props.onClick}>
-        <GoogleLogoSvg width={20} />
+        <GoogleLogoSvg width={20} height={20} />
       </SocialSignInButton>
     </>
-  )
-}
+  );
+};
