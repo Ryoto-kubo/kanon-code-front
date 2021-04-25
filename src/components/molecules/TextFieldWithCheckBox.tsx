45afb6b4da6f21842520b7f68c08a614d6cd68ca
Ryoto-kubo <sax.guiter@icloud.com>
2021-04-25 21:14:43 +0900

    remve checkbox

--- src/components/molecules/TextFieldWithCheckBox.tsx
+++ src/components/molecules/TextFieldWithCheckBox.tsx
@@ -1,27 +1,27 @@
-import Box from "@material-ui/core/Box";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import TextField, { TextFieldProps } from "@material-ui/core/TextField";
-import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
-import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
-import React from "react";
-import styled from "styled-components";
+import Box from '@material-ui/core/Box'
+// import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from '@material-ui/core/IconButton'
+import TextField, { TextFieldProps } from '@material-ui/core/TextField'
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined'
+import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined'
+import React from 'react'
+import styled from 'styled-components'
 
 type Props = TextFieldProps & {
-  index: number;
-  listLength: number;
-  isChecked: boolean;
-  onClick: VoidFunction;
-  onDelete: VoidFunction;
-  onCnangeFileName: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onChangeIsChecked: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onFocusGetIndex: (event: React.FocusEvent<HTMLInputElement>) => void;
-};
+  index: number
+  listLength: number
+  isChecked: boolean
+  onClick: VoidFunction
+  onDelete: VoidFunction
+  onCnangeFileName: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onChangeIsChecked: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onFocusGetIndex: (event: React.FocusEvent<HTMLInputElement>) => void
+}
 
 const StyledIconButton = styled(IconButton)`
   width: 30px;
   height: 30px;
-`;
+`
 
 export const TextFieldWithCheckBox: React.FC<Props> = (props) => {
   const {
@@ -34,16 +34,16 @@ export const TextFieldWithCheckBox: React.FC<Props> = (props) => {
     onChangeIsChecked,
     onFocusGetIndex,
     ...textFieldProps
-  } = props;
+  } = props
   return (
     <>
       <Box mr={1} width="100%" display="flex">
         <Box mr={1}>
-          <Checkbox
+          {/* <Checkbox
             checked={isChecked}
             color="primary"
             onChange={onChangeIsChecked}
-          />
+          /> */}
         </Box>
         <TextField
           {...textFieldProps}
@@ -62,5 +62,5 @@ export const TextFieldWithCheckBox: React.FC<Props> = (props) => {
         </StyledIconButton>
       )}
     </>
-  );
-};
+  )
+}
