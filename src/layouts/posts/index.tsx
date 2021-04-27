7a9dd97ada1804c6530d7a92d71dceb0a1ef57ba
Ryoto-kubo <sax.guiter@icloud.com>
2021-04-27 23:40:48 +0900

    implements redirect

--- src/layouts/posts/index.tsx
+++ src/layouts/posts/index.tsx
@@ -3,22 +3,27 @@ import { CommonHead } from "@/components/common/head/index";
 import { ThePostsHeader } from "@/components/common/header/posts";
 import { Toolbar } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
 type Props = {
   children: ReactNode;
   title: string;
+  authUser: any;
 };
 
 const StyleBoxMain = styled(Box)`
   background: #ffffff;
 `;
 
-const LayoutPosts = ({
-  children,
-  title = "This is the default title",
-}: Props) => {
+const LayoutPosts = ({ children, title, authUser }: Props) => {
+  const router = useRouter();
+  if (authUser === null) {
+    router.push("/");
+    return null;
+  }
+
   return (
     <>
       <CommonHead title={title} />
