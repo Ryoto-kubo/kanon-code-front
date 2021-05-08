708508e32880a474a83a6c76571633e2b54e0883
Ryoto-kubo <sax.guiter@icloud.com>
2021-05-09 01:46:37 +0900

    add nprogress

---
+++ src/utils/get-user.ts
@@ -0,0 +1,10 @@
+import { apis } from '@/consts/api/'
+import { axios } from '@/utils/axios'
+
+type ParamsType = {
+  userId: string
+}
+
+export const getUser = async (params: ParamsType) => {
+  return await axios.get(apis.GET_USER, { params })
+}
