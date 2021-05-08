15dfb8d98f12d58f672b3cd28f2a201dcdeadc47
Ryoto-kubo <sax.guiter@icloud.com>
2021-05-08 17:15:13 +0900

    add console

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,11 +4,10 @@ console.log(process.env.NEXT_PUBLIC_API_URL)
 
 export const axios = baseAxios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 3000,
   // headers: {
   //   'Access-Control-Allow-Headers':
   //     'Origin, Authorization, Accept, X-Requested-With, Content-Type, x-amz-date, X-Amz-Security-Token',
-  // 'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Origin': '*',
   //   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PATCH, PUT',
   // },
 })
