07cbc98f4985d35b309fab5a2feb122aa39bdfd6
Ryoto-kubo <sax.guiter@icloud.com>
2021-05-26 00:47:04 +0900

    change method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,7 +56,7 @@ const IndexPage: React.FC<Props> = (props) => {
 };
 
 // サーバーサイドで実行される
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   try {
     const response = await getContents();
     return {
