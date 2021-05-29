01fb089ce09f46b2f2d18038e86fdcaaf7fbb8e6
Ryoto-kubo <sax.guiter@icloud.com>
2021-05-30 01:44:26 +0900

    create mypage

--- src/components/organisms/ProfileArea.tsx
+++ src/components/organisms/ProfileArea.tsx
@@ -1,84 +1,85 @@
-import { SolidLink } from "@/components/atoms/SolidLink";
-import { SolidLinkSecondary } from "@/components/atoms/SolidLinkSecondary";
-import { UserImgIcon } from "@/components/atoms/UserImgIcon";
-import { UserLinks } from "@/components/organisms/UserLinks";
-import { Box } from "@material-ui/core/";
-import { makeStyles } from "@material-ui/core/styles";
-import Link from "next/link";
-import React from "react";
-import styled from "styled-components";
+import { SolidLink } from '@/components/atoms/SolidLink'
+import { SolidLinkSecondary } from '@/components/atoms/SolidLinkSecondary'
+import { UserImgIcon } from '@/components/atoms/UserImgIcon'
+import { UserLinks } from '@/components/organisms/UserLinks'
+import { positions } from '@/consts/positions'
+import { Box } from '@material-ui/core/'
+import { makeStyles } from '@material-ui/core/styles'
+import Link from 'next/link'
+import React from 'react'
+import styled from 'styled-components'
 
 type Props = {
-  picture: string;
-  position: string;
-  githubName: string;
-  twitterName: string;
-  webSite: string;
-  displayName: string;
-  cognitoId: null | string;
-  isMe: boolean;
-};
+  picture: string
+  position: number
+  githubName: string
+  twitterName: string
+  webSite: string
+  displayName: string
+  cognitoId: null | string
+  isMe: boolean
+}
 
 const useStyles = makeStyles(() => ({
   size: {
-    width: "60px",
-    height: "60px",
+    width: '60px',
+    height: '60px',
   },
-}));
+}))
 
 const StyledBoxProfileArea = styled(Box)`
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     display: flex;
   }
-`;
+`
 const StyledBoxUserProfile = styled(Box)`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     width: 100%;
   }
-`;
+`
 const StyledBoxUserDescription = styled(Box)`
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     display: flex;
     align-items: center;
   }
-`;
+`
 const StyledBoxSwitchMargin = styled(Box)`
   margin-bottom: 3px;
   font-size: 15px;
   color: rgb(136, 153, 166);
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     margin-bottom: 0px;
     margin-right: 8px;
   }
-`;
+`
 const StyledBoxUserAbout = styled(Box)`
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     display: flex;
     align-items: center;
   }
-`;
+`
 const StyledBoxUserName = styled(Box)`
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 3px;
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     margin-bottom: 0px;
   }
-`;
+`
 const StyledBoxFlexDirection = styled(Box)`
   flex-direction: column-reverse;
   display: flex;
-  ${(props) => props.theme.breakpoints.up("sm")} {
+  ${(props) => props.theme.breakpoints.up('sm')} {
     flex-direction: unset;
     display: block;
   }
-`;
+`
 
 export const ProfileArea: React.FC<Props> = (props) => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <StyledBoxProfileArea component="section">
@@ -94,7 +95,7 @@ export const ProfileArea: React.FC<Props> = (props) => {
           <StyledBoxFlexDirection>
             <StyledBoxUserAbout>
               <StyledBoxSwitchMargin>
-                <p>{props.position}</p>
+                <p>{positions[props.position].label}</p>
               </StyledBoxSwitchMargin>
               <UserLinks
                 githubName={props.githubName}
@@ -118,5 +119,5 @@ export const ProfileArea: React.FC<Props> = (props) => {
           ))}
       </StyledBoxUserProfile>
     </StyledBoxProfileArea>
-  );
-};
+  )
+}
