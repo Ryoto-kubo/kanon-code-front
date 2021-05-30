97b15eca4331a246ab6f76fa6b70b4168817a02b
Ryoto-kubo <sax.guiter@icloud.com>
2021-05-30 15:04:46 +0900

    fetched user data

---
+++ src/types/pages/settings/profile/index.ts
@@ -0,0 +1,11 @@
+export type UserProfileProps = {
+  display_name: string
+  github_name: string
+  icon_src: string
+  introduction: string
+  position_type: number
+  price: number
+  skils: []
+  twitter_name: string
+  web_site: string
+}
