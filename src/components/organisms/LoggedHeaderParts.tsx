51da674c30b49ff65956e5632d7bbb1d394ea444
Ryoto-kubo <sax.guiter@icloud.com>
2021-06-06 00:05:13 +0900

    render test

--- src/components/organisms/LoggedHeaderParts.tsx
+++ src/components/organisms/LoggedHeaderParts.tsx
@@ -14,7 +14,7 @@ import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined"
 import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
 import SpeedOutlinedIcon from "@material-ui/icons/SpeedOutlined";
 import { Auth } from "aws-amplify";
-import Link from "next/link";
+// import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import styled from "styled-components";
@@ -35,12 +35,12 @@ const StyledUseMr = styled.span`
 const StyledListItemIcon = styled(ListItemIcon)`
   min-width: 36px;
 `;
-const StyledAnchor = styled(ListItemIcon)`
-display: flex;
-text-decoration: none;
-align-items: center;
-}
-`;
+// const StyledAnchor = styled(ListItemIcon)`
+// display: flex;
+// text-decoration: none;
+// align-items: center;
+// }
+// `;
 
 export const LoggedHeaderParts: React.FC<Props> = (props) => {
   const router = useRouter();
@@ -102,27 +102,17 @@ export const LoggedHeaderParts: React.FC<Props> = (props) => {
           open={open}
           onClose={handleClose}
         >
-          {/* <MenuItem> */}
           <MenuItem onClick={() => toPage(`/${props.displayName}`)}>
-            {/* <Link prefetch href={`${props.displayName}`} passHref>
-              <StyledAnchor> */}
             <StyledListItemIcon>
               <PersonOutlineOutlinedIcon fontSize="small" />
             </StyledListItemIcon>
             <ListItemText secondary="マイページ" />
-            {/* </StyledAnchor>
-            </Link> */}
           </MenuItem>
-          <MenuItem>
-            {/* <MenuItem onClick={() => toPage("/post/new")}> */}
-            <Link prefetch href="/post/new" passHref>
-              <StyledAnchor>
+            <MenuItem onClick={() => toPage("/post/new")}>
                 <StyledListItemIcon>
                   <CodeOutlinedIcon fontSize="small" />
                 </StyledListItemIcon>
                 <ListItemText secondary="レビュー依頼" />
-              </StyledAnchor>
-            </Link>
           </MenuItem>
           <Divider />
           <MenuItem onClick={() => toPage("/dashboard/reviews")}>
