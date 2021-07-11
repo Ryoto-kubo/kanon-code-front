0359e64e113757f129bbdb2af8064663046d3c76
Ryoto-kubo <sax.guiter@icloud.com>
2021-07-11 17:54:20 +0900

    add the logic for post

--- src/utils/api/post-content.ts
+++ src/utils/api/post-content.ts
@@ -1,37 +1,36 @@
-import { apis } from '@/consts/api/'
-import { axios } from '@/utils/axios'
+import { apis } from "@/consts/api/";
+import { axios } from "@/utils/axios";
 
 type ParamsType = {
-  uuid: string
-  userId: string
+  uuid: string;
   userProfile: {
-    display_name: string
-    github_name: string
-    icon_src: string
-    introduction: string
-    position_type: number
-    price: number
-    twitter_name: string
-    web_site: string
-  }
-  postType: string
+    display_name: string;
+    github_name: string;
+    icon_src: string;
+    introduction: string;
+    position_type: number;
+    price: number;
+    twitter_name: string;
+    web_site: string;
+  };
+  postType: string;
   contents: {
-    title: string
-    tagList: string[]
+    title: string;
+    tagList: string[];
     description: {
-      value: string
-      bodyHtml: string
-    }
+      value: string;
+      bodyHtml: string;
+    };
     inputFileNameLists: {
-      key: string
-      fileName: string
-      sourceCode: string
-      bodyHtml: string
-      isValid: boolean
-    }[]
-  }
-}
+      key: string;
+      fileName: string;
+      sourceCode: string;
+      bodyHtml: string;
+      isValid: boolean;
+    }[];
+  };
+};
 
 export const postContent = async (params: ParamsType) => {
-  return await axios.post(apis.REGISTER_CONTENT, params)
-}
+  return await axios.post(apis.REGISTER_CONTENT, params);
+};
