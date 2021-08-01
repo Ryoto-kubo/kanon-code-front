1ca0c997cef4b6712f5cc385c113e64d472c39f7
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-08-01 18:53:01 +0900

    refactoring mypage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,63 @@
-import { TypoHeading2 } from '@/components/atoms/TypoHeading2'
+import { TypoHeading2 } from '@/components/atoms/TypoHeading2';
 // import { CustomLoader } from "@/components/common/loader";
-import { FirstView } from '@/components/organisms/FirstView'
-import { Post } from '@/components/organisms/Post'
-import Layout from '@/layouts/standard'
-import theme from '@/styles/theme'
-import { UserTypes } from '@/types/global'
-import { PostContentsTypes } from '@/types/global/'
-import { getContents } from '@/utils/api/get-contents'
-import { Box, Container, Grid } from '@material-ui/core/'
-import React from 'react'
-import styled from 'styled-components'
-import { v4 as uuidv4 } from 'uuid'
+import { FirstView } from '@/components/organisms/FirstView';
+import { Post } from '@/components/organisms/Post';
+import Layout from '@/layouts/standard';
+import theme from '@/styles/theme';
+import { UserTypes } from '@/types/global';
+import { GetContentsTypes } from '@/types/global/';
+import { getContents } from '@/utils/api/get-contents';
+import { Box, Container, Grid } from '@material-ui/core/';
+import React from 'react';
+import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
 
 type Props = {
-  authUser: any
-  currentUser: UserTypes | null
+  authUser: any;
+  currentUser: UserTypes | null;
   data: {
-    Count: number
-    frontPosts: PostContentsTypes[]
-    backPosts: PostContentsTypes[]
-    otherPosts: PostContentsTypes[]
-    ScannedCount: number
-  }
-}
+    Count: number;
+    frontPosts: GetContentsTypes[];
+    backPosts: GetContentsTypes[];
+    otherPosts: GetContentsTypes[];
+    ScannedCount: number;
+  };
+};
 
 const StyledBoxWidthBorder = styled(Box)`
   border-left: 3px solid ${theme.palette.primary.main};
   padding-left: 8px;
-`
+`;
 
-const IndexPage: React.FC<Props> = (props) => {
-  const frontPosts = props.data.frontPosts
-  const backPosts = props.data.backPosts
-  const otherPosts = props.data.otherPosts
+const IndexPage: React.FC<Props> = props => {
+  console.log(props.data);
+
+  const frontPosts = props.data.frontPosts;
+  const backPosts = props.data.backPosts;
+  const otherPosts = props.data.otherPosts;
   return (
     <Layout
-      title="Kanon Code | コードレビュを全てのエンジニアへ"
+      title='Kanon Code | コードレビュを全てのエンジニアへ'
       currentUser={props.currentUser}
     >
       <Container>
         {!props.currentUser && <FirstView />}
-        <Box component="section" mb={5}>
+        <Box component='section' mb={5}>
           {frontPosts.length > 0 && (
             <StyledBoxWidthBorder mb={2}>
-              <TypoHeading2 color="initial">Front-end</TypoHeading2>
+              <TypoHeading2 color='initial'>Front-end</TypoHeading2>
             </StyledBoxWidthBorder>
           )}
           <Box mb={4}>
             <Grid spacing={3} container>
-              {frontPosts.map((el) => (
+              {frontPosts.map(el => (
                 <Grid item xs={12} sm={6} md={6} lg={4} key={uuidv4()}>
                   <Post
                     title={el.contents.title}
                     postUrl={el.post_url}
                     iconPath={el.contents.target_icon.icon_path}
                     name={el.user_profile.display_name}
-                    date={`${el.create_year}/${el.create_month}/${el.create_day}`}
+                    date={el.date}
                     tagArray={el.contents.tag_list}
                     userIcon={el.user_profile.icon_src}
                   />
@@ -64,22 +66,22 @@ const IndexPage: React.FC<Props> = (props) => {
             </Grid>
           </Box>
         </Box>
-        <Box component="section" mb={5}>
+        <Box component='section' mb={5}>
           {backPosts.length > 0 && (
             <StyledBoxWidthBorder mb={2}>
-              <TypoHeading2 color="initial">Back-end</TypoHeading2>
+              <TypoHeading2 color='initial'>Back-end</TypoHeading2>
             </StyledBoxWidthBorder>
           )}
           <Box mb={4}>
             <Grid spacing={3} container>
-              {backPosts.map((el) => (
+              {backPosts.map(el => (
                 <Grid item xs={12} sm={6} md={6} lg={4} key={uuidv4()}>
                   <Post
                     title={el.contents.title}
                     postUrl={el.post_url}
                     iconPath={el.contents.target_icon.icon_path}
                     name={el.user_profile.display_name}
-                    date={`${el.create_year}/${el.create_month}/${el.create_day}`}
+                    date={el.date}
                     tagArray={el.contents.tag_list}
                     userIcon={el.user_profile.icon_src}
                   />
@@ -88,22 +90,22 @@ const IndexPage: React.FC<Props> = (props) => {
             </Grid>
           </Box>
         </Box>
-        <Box component="section" mb={5}>
+        <Box component='section' mb={5}>
           {otherPosts.length > 0 && (
             <StyledBoxWidthBorder mb={2}>
-              <TypoHeading2 color="initial">Other</TypoHeading2>
+              <TypoHeading2 color='initial'>Other</TypoHeading2>
             </StyledBoxWidthBorder>
           )}
           <Box mb={4}>
             <Grid spacing={3} container>
-              {otherPosts.map((el) => (
+              {otherPosts.map(el => (
                 <Grid item xs={12} sm={6} md={6} lg={4} key={uuidv4()}>
                   <Post
                     title={el.contents.title}
                     postUrl={el.post_url}
                     iconPath={el.contents.target_icon.icon_path}
                     name={el.user_profile.display_name}
-                    date={`${el.create_year}/${el.create_month}/${el.create_day}`}
+                    date={el.date}
                     tagArray={el.contents.tag_list}
                     userIcon={el.user_profile.icon_src}
                   />
@@ -114,26 +116,26 @@ const IndexPage: React.FC<Props> = (props) => {
         </Box>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 // サーバーサイドで実行される
 export const getStaticProps = async () => {
   // export const getServerSideProps = async () => {
   try {
-    const response = await getContents()
+    const response = await getContents();
     return {
       props: {
         data: response.data,
       },
       revalidate: 30,
-    }
+    };
   } catch (error) {
     return {
       props: {
         data: null,
       },
-    }
+    };
   }
-}
+};
 
-export default IndexPage
+export default IndexPage;
