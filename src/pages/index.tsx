9ab191baa265b115f9d0e91eff0a8bd5280fb959
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-08-27 01:17:50 +0900

    render test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -121,6 +121,8 @@ export const getStaticProps = async () => {
   // export const getServerSideProps = async () => {
   try {
     const response = await getContents();
+    console.log(response);
+
     return {
       props: {
         data: response.data,
