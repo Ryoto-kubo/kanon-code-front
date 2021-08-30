407b627eeeda303688bd71f2d29dc0b54a2854f0
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-08-30 20:35:38 +0900

    add the meta tag

--- src/components/common/head/index.tsx
+++ src/components/common/head/index.tsx
@@ -15,17 +15,22 @@ export const CommonHead = ({ title = 'This is the default title' }: Props) => (
     <meta property='og:description' content='Kanon Code description' />
     <meta property='og:url' content='https://stg.kanon-code.com' />
     <meta property='og:site_name' content='Kanon Code' />
-    <meta property='og:image' content='https://stg.kanon-code.com/logo.png' />
+    <meta
+      property='og:image'
+      content='https://stg-contents-kanon-code.s3-ap-northeast-1.amazonaws.com/icons/angular.svg'
+    />
     <meta property='og:image:width' content={String(1280)} />
     <meta property='og:image:height' content={String(960)} />
     <meta name='twitter:card' content='summary' />
     <meta name='twitter:site' content='@tcr_jp' />
-    <meta name='twitter:url' content={'https://stg.kanon-code.com/logo.png'} />
+    <meta name='twitter:url' content={'https://stg.kanon-code.com'} />
     <meta name='twitter:title' content={'Kanon Code'} />
     <meta name='twitter:description' content={'description'} />
     <meta
       name='twitter:image'
-      content={'https://stg.kanon-code.com/logo.png'}
+      content={
+        'https://stg-contents-kanon-code.s3-ap-northeast-1.amazonaws.com/icons/angular.svg'
+      }
     />
   </Head>
 );
