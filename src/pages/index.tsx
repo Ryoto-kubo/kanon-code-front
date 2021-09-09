c887d4679e01642454297121d14187ee5a1a4e13
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-09-10 01:43:07 +0900

    test ogp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -122,7 +122,6 @@ const IndexPage: React.FC<Props> = props => {
 };
 // サーバーサイドで実行される
 export const getStaticProps = async () => {
-  // export const getServerSideProps = async () => {
   try {
     const response = await getContents();
     return {
