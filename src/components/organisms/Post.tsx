541d897063dceb43012462cd533f4a3c0f4d090e
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-09-10 23:14:53 +0900

    create style for price

--- src/components/organisms/Post.tsx
+++ src/components/organisms/Post.tsx
@@ -4,6 +4,8 @@ import { PostHeader } from '@/components/molecules/PostHeader';
 import { ACCEPT_REVIEW, STOP_REVIEW } from '@/consts/const';
 import { Box } from '@material-ui/core/';
 import Paper from '@material-ui/core/Paper';
+import { alpha } from '@material-ui/core/styles';
+import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
 import React from 'react';
 import styled from 'styled-components';
 import { StopReviewIcon } from '../atoms/StopReviewIcon';
@@ -21,6 +23,7 @@ interface Props {
 
 const StyledPaper = styled(Paper)`
   height: 100%;
+  position: relative;
 `;
 const StyledBoxWraper = styled(Box)`
   min-height: 175px;
@@ -30,11 +33,28 @@ const StyledBoxContent = styled(Box)`
   display: flex;
   align-items: center;
 `;
+const StyledBoxBudget = styled(Box)`
+  color: #fb8c00;
+  background: ${alpha('#fb8c00', 0.1)};
+  border-radius: 4px 0 4px 0;
+  padding: 4px 8px;
+  font-weight: bold;
+  font-size: 12px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+`;
 
 export const Post: React.FC<Props> = props => {
   return (
     <StyledPaper>
       <StyledBoxWraper>
+        <StyledBoxBudget>
+          <MonetizationOnOutlinedIcon fontSize='small' />
+          <Box ml={0.5}>~¥3000</Box>
+        </StyledBoxBudget>
         <Box textAlign='right' mb={1}>
           {props.postStatus === ACCEPT_REVIEW ? (
             <AcceptReviewIcon />
