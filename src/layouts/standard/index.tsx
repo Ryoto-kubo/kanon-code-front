c8a99ace72b544be4b98599362cc3ab827f4f49c
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-09-12 23:42:48 +0900

    Remove console.log

--- src/layouts/standard/index.tsx
+++ src/layouts/standard/index.tsx
@@ -21,43 +21,11 @@ const StyleBoxMain = styled(Box)`
   background: #ffffff;
 `;
 
-const Layout: React.FC<Props> = ({ children, title, currentUser }) => {
-  console.log(title);
-
+const Layout: React.FC<Props> = ({ children, currentUser }) => {
   const displayName = currentUser?.user_profile.display_name;
   const { isOpenSignin, setIsOpenSignin } = useIsOpenSignin();
   return (
     <>
-      {/* <Head>
-        <title>{title}</title>
-        <meta
-          name='viewport'
-          content='width=device-width,height=device-height'
-          key='viewport'
-        />
-        <meta property='og:title' content='Kanon Code | テスト' />
-        <meta property='og:type' content='website' />
-        <meta property='og:description' content='Kanon テスト' />
-        <meta property='og:url' content='https://stg.kanon-code.com' />
-        <meta property='og:site_name' content='Kanon Code' />
-        <meta
-          property='og:image'
-          content='https://stg-contents-kanon-code.s3-ap-northeast-1.amazonaws.com/icons/angular.svg'
-        />
-        <meta property='og:image:width' content={String(1280)} />
-        <meta property='og:image:height' content={String(960)} />
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:site' content='@kanon_code_com' />
-        <meta name='twitter:url' content={'https://stg.kanon-code.com'} />
-        <meta name='twitter:title' content={'Kanon Code'} />
-        <meta name='twitter:description' content={'Kanon テスト'} />
-        <meta
-          name='twitter:image'
-          content={
-            'https://stg-contents-kanon-code.s3-ap-northeast-1.amazonaws.com/icons/angular.svg'
-          }
-        />
-      </Head> */}
       {displayName === '' ? (
         <TheRegisterHeader />
       ) : currentUser ? (
