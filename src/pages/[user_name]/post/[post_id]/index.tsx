29d1f5458223f81b9d0034e8fc10851fe6bb8737
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-09-12 14:05:07 +0900

    ogp test

--- src/pages/[user_name]/post/[post_id]/index.tsx
+++ src/pages/[user_name]/post/[post_id]/index.tsx
@@ -139,7 +139,8 @@ const IndexPage: React.FC<Props> = props => {
       <CommonHead
         title={`Kanon Code | ${title}`}
         description={props.post ? props.post.contents.description.value : ''}
-        image={`${process.env.NEXT_PUBLIC_BUCKET_URL}${iconPath}`}
+        image={`${process.env.NEXT_PUBLIC_BUCKET_URL}icons/ogps/docker.png`}
+        // image={`${process.env.NEXT_PUBLIC_BUCKET_URL}${iconPath}`}
       />
       <StyledBoxBgGray>
         <StyledContainer maxWidth='md'>
