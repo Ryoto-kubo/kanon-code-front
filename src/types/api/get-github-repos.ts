3e14d04c5ebc17c02368e92dc1499f7f67b36bb6
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-10-04 02:49:55 +0900

    Fix typo

--- src/types/api/get-github-repos.ts
+++ src/types/api/get-github-repos.ts
@@ -1,6 +1,6 @@
-import { GihubReposTypes } from '@/types/global/index';
+import { GithubReposTypes } from '@/types/global/index';
 export type ResponseGithubReposTypes = {
-  repos: GihubReposTypes[];
+  repos: GithubReposTypes[];
   status: boolean;
   status_code: number;
   status_message: string;
