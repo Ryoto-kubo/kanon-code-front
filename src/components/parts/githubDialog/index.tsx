c2af065d7bc1676de39df94c28be000af837a633
久保諒翔 <kuboryoto@kuboryoushounoiMac.local>
2021-10-06 18:32:16 +0900

    Implements github dialog

--- src/components/parts/githubDialog/index.tsx
+++ src/components/parts/githubDialog/index.tsx
@@ -8,6 +8,7 @@ import { ResponseGithubSourceTreeTypes } from '@/types/api/get-github-source-tre
 import { GithubReposTypes } from '@/types/global/index';
 import { IconButton, TextField } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import Chip from '@material-ui/core/Chip';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import Slide from '@material-ui/core/Slide';
@@ -28,6 +29,12 @@ type Props = {
     repository: string,
     branch: string
   ) => Promise<AxiosResponse<ResponseGithubSourceTreeTypes>>;
+  insertToInputFileNameLists: (
+    choosedFullPathList: string[],
+    encodeContents: {
+      [key: string]: string;
+    }
+  ) => void;
   closeDialog: () => void;
 };
 
@@ -45,7 +52,8 @@ const StyledBoxWrapper = styled(Box)`
 `;
 const StyledBoxContetntWrapper = styled(Box)`
   display: block;
-  ${props => props.theme.breakpoints.up('sm')} {
+  margin-bottom: 16px;
+  ${props => props.theme.breakpoints.up('md')} {
     display: flex;
     justify-content: space-between;
   }
@@ -53,9 +61,11 @@ const StyledBoxContetntWrapper = styled(Box)`
 const StyledBoxSettingWrapper = styled(Box)`
   width: 100%;
   margin-bottom: 24px;
-  ${props => props.theme.breakpoints.up('sm')} {
+  min-height: 290px;
+  ${props => props.theme.breakpoints.up('md')} {
     width: 30%;
     max-width: 320px;
+    margin-right: 24px;
   }
 `;
 const StyledBoxAutoCompleteChild = styled(Box)`
@@ -67,14 +77,36 @@ const StyledBoxGetCodeButton = styled(Box)`
 `;
 const StyledBoxTreeWrapper = styled(Box)`
   position: relative;
-  padding-bottom: 20px;
+  min-height: 30px;
+  max-height: 500px;
+  overflow-y: scroll;
 `;
 const StyledBoxSourceCodeWrapper = styled(Box)`
+  position: relative;
   text-align: left;
   width: 100%;
-  ${props => props.theme.breakpoints.up('sm')} {
-    width: 77%;
+`;
+const StyledBoxTitleWrapper = styled(Box)`
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 8px;
+`;
+const StyledSelectedSourceCodeWrapper = styled(Box)`
+  margin-bottom: 16px;
+  > * {
+    margin-right: 8px;
+    margin-bottom: 8px;
   }
+`;
+const StyledSourceCodeWrapper = styled(Box)`
+  height: 550px;
+  max-height: 550px;
+  overflow-y: scroll;
+  position: relative;
+  margin-bottom: 16px;
+`;
+const StyledBoxApplyButtonWrapper = styled(Box)`
+  text-align: right;
 `;
 
 const Transition = React.forwardRef(function Transition(
@@ -91,26 +123,39 @@ export const GithubDialog: React.FC<Props> = props => {
     isOpen,
     getBranches,
     getSourceTreeByBranch,
+    insertToInputFileNameLists,
     closeDialog,
   } = props;
 
   const {
     choosedRepository,
-    choosedBranch,
-    isBranchFetch,
-    isTreeFetch,
-    treeObject,
-    decodedContent,
+    choosedFullPathList,
     isChoosedRepository,
     isChoosedBranch,
+    isChoosedFullPath,
+    isFetchBranch,
+    isFetchTree,
+    isFetchContent,
+    treeObject,
+    decodedContent,
+    currentSelectedPath,
     getBranchesBySelectedRepo,
     getChoosedRepositoryBranches,
     selectBranch,
     getTree,
     getContent,
-  } = useGithubDialog(repos, getBranches, getSourceTreeByBranch);
+    getKeyForTree,
+    changeDisplaySourceCode,
+    deleteSelctedSourceCode,
+    applySourceCode,
+  } = useGithubDialog(
+    repos,
+    getBranches,
+    getSourceTreeByBranch,
+    insertToInputFileNameLists
+  );
 
-  const key = `${choosedRepository}#${choosedBranch}`;
+  const key = getKeyForTree();
   return (
     <Dialog
       open={isOpen}
@@ -119,7 +164,7 @@ export const GithubDialog: React.FC<Props> = props => {
       fullScreen
       fullWidth
       maxWidth='sm'
-      onClose={closeDialog}
+      onClose={() => closeDialog()}
       aria-labelledby='alert-dialog-slide-title'
       aria-describedby='alert-dialog-slide-description'
     >
@@ -130,7 +175,7 @@ export const GithubDialog: React.FC<Props> = props => {
         <IconButton
           edge='start'
           color='inherit'
-          onClick={closeDialog}
+          onClick={() => closeDialog()}
           aria-label='close'
         >
           <CloseIcon />
@@ -138,96 +183,135 @@ export const GithubDialog: React.FC<Props> = props => {
       </StyledBoxHeaderWrapper>
       <StyledBoxWrapper>
         <DialogContent>
-          <StyledBoxContetntWrapper>
-            <StyledBoxSettingWrapper>
-              {isFetch ? (
-                <CustomLoader width={30} height={30} />
-              ) : repos.length <= 0 ? (
-                <p>レポジトリが存在しません。</p>
-              ) : (
-                <>
-                  <StyledBoxAutoCompleteChild>
-                    <Box mb={0.5}>
-                      <Autocomplete
-                        disablePortal
-                        autoHighlight
-                        autoSelect
-                        id='repository'
-                        options={repos}
-                        style={{ width: '100%' }}
-                        getOptionLabel={option => option.name}
-                        renderInput={(params: any) => (
-                          <TextField
-                            {...params}
-                            label='Repository'
-                            error={!isChoosedRepository}
+          {isFetch ? (
+            <CustomLoader width={30} height={30} />
+          ) : (
+            <>
+              <StyledBoxContetntWrapper>
+                <StyledBoxSettingWrapper>
+                  {repos.length <= 0 ? (
+                    <p>レポジトリが存在しません。</p>
+                  ) : (
+                    <>
+                      <StyledBoxAutoCompleteChild>
+                        <Box mb={0.5}>
+                          <Autocomplete
+                            disablePortal
+                            autoHighlight
+                            autoSelect
+                            id='repository'
+                            options={repos}
+                            style={{ width: '100%' }}
+                            getOptionLabel={option => option.name}
+                            renderInput={(params: any) => (
+                              <TextField
+                                {...params}
+                                label='Repository'
+                                error={!isChoosedRepository}
+                              />
+                            )}
+                            onChange={getBranchesBySelectedRepo}
+                          />
+                        </Box>
+                        {!isChoosedRepository && (
+                          <ValidMessage validText='リポジトリを選択してください' />
+                        )}
+                      </StyledBoxAutoCompleteChild>
+                      <StyledBoxAutoCompleteChild>
+                        <Box mb={0.5}>
+                          <Autocomplete
+                            disablePortal
+                            autoHighlight
+                            autoSelect
+                            id='branch'
+                            options={getChoosedRepositoryBranches(
+                              choosedRepository
+                            )}
+                            style={{ width: '100%' }}
+                            getOptionLabel={option => option.name}
+                            renderInput={(params: any) => (
+                              <TextField
+                                {...params}
+                                label='Branch'
+                                error={!isChoosedBranch}
+                              />
+                            )}
+                            loading={isFetchBranch}
+                            onChange={selectBranch}
+                          />
+                        </Box>
+                        {!isChoosedBranch && (
+                          <ValidMessage validText='ブランチを選択してください' />
+                        )}
+                      </StyledBoxAutoCompleteChild>
+                      <StyledBoxGetCodeButton>
+                        <CustomSolidButton
+                          sizing='small'
+                          color='primary'
+                          onClick={getTree}
+                        >
+                          コードを取得
+                        </CustomSolidButton>
+                      </StyledBoxGetCodeButton>
+                      <StyledBoxTreeWrapper>
+                        {isFetchTree ? (
+                          <CustomLoader width={25} height={25} />
+                        ) : (
+                          <GithubSourceTree
+                            sourceTreeObject={treeObject[key]}
+                            getContent={getContent}
                           />
                         )}
-                        onChange={getBranchesBySelectedRepo}
+                      </StyledBoxTreeWrapper>
+                    </>
+                  )}
+                </StyledBoxSettingWrapper>
+                <StyledBoxSourceCodeWrapper>
+                  <StyledBoxTitleWrapper>
+                    Selected Source Code
+                  </StyledBoxTitleWrapper>
+                  {!isChoosedFullPath && (
+                    <ValidMessage validText='ファイルを選択してください' />
+                  )}
+                  <StyledSelectedSourceCodeWrapper>
+                    {choosedFullPathList.map(path => (
+                      <Chip
+                        key={path}
+                        label={path}
+                        color='primary'
+                        onClick={() => changeDisplaySourceCode(path)}
+                        onDelete={() => deleteSelctedSourceCode(path)}
+                        variant={
+                          currentSelectedPath === path ? 'default' : 'outlined'
+                        }
                       />
-                    </Box>
-                    {!isChoosedRepository && (
-                      <ValidMessage validText='リポジトリを選択してください' />
-                    )}
-                  </StyledBoxAutoCompleteChild>
-                  <StyledBoxAutoCompleteChild>
-                    <Box mb={0.5}>
-                      <Autocomplete
-                        disablePortal
-                        autoHighlight
-                        autoSelect
-                        id='branch'
-                        options={getChoosedRepositoryBranches(
-                          choosedRepository
-                        )}
-                        style={{ width: '100%' }}
-                        getOptionLabel={option => option.name}
-                        renderInput={(params: any) => (
-                          <TextField
-                            {...params}
-                            label='Branch'
-                            error={!isChoosedBranch}
-                          />
-                        )}
-                        loading={isBranchFetch}
-                        onChange={selectBranch}
+                    ))}
+                  </StyledSelectedSourceCodeWrapper>
+                  <StyledSourceCodeWrapper>
+                    {isFetchContent ? (
+                      <CustomLoader width={25} height={25} />
+                    ) : (
+                      <span
+                        className='decode-item-wrapper'
+                        dangerouslySetInnerHTML={{
+                          __html: marked(decodedContent),
+                        }}
                       />
-                    </Box>
-                    {!isChoosedBranch && (
-                      <ValidMessage validText='ブランチを選択してください' />
                     )}
-                  </StyledBoxAutoCompleteChild>
-                  <StyledBoxGetCodeButton>
+                  </StyledSourceCodeWrapper>
+                  <StyledBoxApplyButtonWrapper>
                     <CustomSolidButton
                       sizing='small'
                       color='primary'
-                      onClick={getTree}
+                      onClick={() => applySourceCode()}
                     >
-                      コードを取得
+                      エディタに反映させる
                     </CustomSolidButton>
-                  </StyledBoxGetCodeButton>
-                  <StyledBoxTreeWrapper>
-                    {isTreeFetch ? (
-                      <CustomLoader width={25} height={25} />
-                    ) : (
-                      <GithubSourceTree
-                        sourceTree={treeObject[key]}
-                        getContent={getContent}
-                      />
-                    )}
-                  </StyledBoxTreeWrapper>
-                </>
-              )}
-            </StyledBoxSettingWrapper>
-            <StyledBoxSourceCodeWrapper>
-              <span
-                className='decode-item-wrapper'
-                dangerouslySetInnerHTML={{
-                  __html: marked(decodedContent),
-                }}
-              />
-            </StyledBoxSourceCodeWrapper>
-          </StyledBoxContetntWrapper>
+                  </StyledBoxApplyButtonWrapper>
+                </StyledBoxSourceCodeWrapper>
+              </StyledBoxContetntWrapper>
+            </>
+          )}
         </DialogContent>
       </StyledBoxWrapper>
     </Dialog>
--- src/hooks/useGithubDialog.ts
+++ src/hooks/useGithubDialog.ts
@@ -1,13 +1,18 @@
 import { errorMessages } from '@/consts/error-messages';
 import { ResponseGithubSourceTreeTypes } from '@/types/api/get-github-source-tree';
-import { SourceTreeTypes } from '@/types/global';
+import { GithubSourceTreeTypes } from '@/types/global';
 import { GithubBranchesTypes, GithubReposTypes } from '@/types/global/index';
 import { getGithubEncodeContent } from '@/utils/api/get-github-encode-content';
 import { AxiosResponse } from 'axios';
 import { useState } from 'react';
 
 type TreeTypes = {
-  [key: string]: SourceTreeTypes[];
+  [key: string]: {
+    tree: GithubSourceTreeTypes[];
+    contents: {
+      [key: string]: string;
+    };
+  };
 };
 
 export const useGithubDialog = (
@@ -16,14 +21,24 @@ export const useGithubDialog = (
   getSourceTreeByBranch: (
     repository: string,
     branch: string
-  ) => Promise<AxiosResponse<ResponseGithubSourceTreeTypes>>
+  ) => Promise<AxiosResponse<ResponseGithubSourceTreeTypes>>,
+  insertToInputFileNameLists: (
+    choosedFullPathList: string[],
+    encodeContents: {
+      [key: string]: string;
+    }
+  ) => void
 ) => {
   const [choosedRepository, setChoosedRepository] = useState('');
   const [choosedBranch, setChoosedBranch] = useState('');
+  const [choosedFullPathList, setChoosedFullPathList] = useState<string[]>([]);
+  const [currentSelectedPath, setCurrentSelectedPath] = useState('');
   const [isChoosedRepository, setIsChoosedRepository] = useState(true);
   const [isChoosedBranch, setIsChoosedBranch] = useState(true);
-  const [isBranchFetch, setIsBranchFetch] = useState(false);
-  const [isTreeFetch, setIsTreeFetch] = useState(false);
+  const [isChoosedFullPath, setIsChoosedFullPath] = useState(true);
+  const [isFetchBranch, setIsFetchBranch] = useState(false);
+  const [isFetchTree, setIsFetchTree] = useState(false);
+  const [isFetchContent, setIsFetchContent] = useState(false);
   const [treeObject, setTreeObject] = useState<TreeTypes>({});
   const [decodedContent, setDecodedContent] = useState('');
 
@@ -47,12 +62,14 @@ export const useGithubDialog = (
     setChoosedBranch('');
     setIsChoosedRepository(true);
     setChoosedRepository(currentRepoFullName);
+    setChoosedFullPathList([]);
+    setDecodedContent('');
     const branches = getChoosedRepositoryBranches(currentRepoFullName);
     // 選択したリポジトリにブランチが存在する場合は、過去に選択されたリポジトリなので、apiを叩かない
     if (branches.length > 0) return;
-    setIsBranchFetch(true);
+    setIsFetchBranch(true);
     await getBranches(currentRepoFullName);
-    setIsBranchFetch(false);
+    setIsFetchBranch(false);
   };
 
   const selectBranch = (
@@ -71,8 +88,9 @@ export const useGithubDialog = (
   const decodeContent = (name: string, encodeContent: string) => {
     const lang = name.split('.').pop();
     const decodeContent = window.atob(encodeContent);
-    const bedginMd = `\`\`\`\`${lang}\n`;
-    const mdContent = `${bedginMd}${decodeContent}`;
+    const bedginMd = `\`\`\`${lang}\n`;
+    const endMd = '\n```';
+    const mdContent = `${bedginMd}${decodeContent}${endMd}`;
     setDecodedContent(mdContent.replace(/ /g, '\u00A0'));
   };
 
@@ -82,56 +100,146 @@ export const useGithubDialog = (
     return choosedRepository !== '' && choosedBranch !== '';
   };
 
+  const validChoosedFullPath = () => {
+    const isValid = choosedFullPathList.length > 0;
+    setIsChoosedFullPath(isValid);
+    return isValid;
+  };
+
   const getTree = async () => {
     const isValid = validSelectedRepositoryAndBranch();
     if (!isValid) return;
     const key = `${choosedRepository}#${choosedBranch}`;
     // すでにtreeを取得ずみのrepository & branchの組み合わせはreturnしておく
     if (treeObject[key] !== undefined) return;
-    setIsTreeFetch(true);
+    setIsFetchTree(true);
     try {
       const result = await getSourceTreeByBranch(
         choosedRepository,
         choosedBranch
       );
       const tree = result.data.tree.children!;
-      setTreeObject({ ...treeObject, [key]: tree });
-      setIsTreeFetch(false);
+      setTreeObject({
+        ...treeObject,
+        [key]: {
+          tree: tree,
+          contents: {},
+        },
+      });
+      setIsFetchTree(false);
     } catch (error) {
       console.error(error);
       alert(errorMessages.SYSTEM_ERROR);
-      setIsTreeFetch(false);
+      setIsFetchTree(false);
     }
   };
 
-  const getContent = async (name: string, sha: string) => {
+  const getContent = async (name: string, sha: string, path: string) => {
+    const key = getKeyForTree();
     const isValid = validSelectedRepositoryAndBranch();
     if (!isValid) return;
     if (sha === undefined) return;
+    const savedEncodeContent = treeObject[key].contents[path];
+    if (savedEncodeContent !== undefined) {
+      // 一度取得したコードはstateに持たせる
+      decodeContent(name, savedEncodeContent);
+      insertPathTochoosedFullPathListIfNeeded(path);
+      changeDisplaySourceCode(path);
+      return;
+    }
     const repository = choosedRepository;
+    const newContents = treeObject[key].contents;
+    setIsFetchContent(true);
+    setChoosedFullPathList([...choosedFullPathList, path]);
     try {
       const result = await getGithubEncodeContent({ repository, sha });
       const encodeContent = result.data.content;
       decodeContent(name, encodeContent);
+      newContents[path] = encodeContent;
+      setTreeObject({
+        ...treeObject,
+        [key]: {
+          tree: treeObject[key].tree,
+          contents: newContents,
+        },
+      });
+      setIsChoosedFullPath(true);
+      setCurrentSelectedPath(path);
+      changeDisplaySourceCode(path);
+      setIsFetchContent(false);
     } catch (error) {
       console.error(error);
       alert(errorMessages.SYSTEM_ERROR);
+      setIsFetchContent(false);
     }
+  };
+
+  const getKeyForTree = () => {
+    return `${choosedRepository}#${choosedBranch}`;
+  };
+
+  const insertPathTochoosedFullPathListIfNeeded = (path: string) => {
+    const newChoosedFullPathList = choosedFullPathList.filter(
+      el => el === path
+    );
+    if (newChoosedFullPathList.length <= 0) {
+      setCurrentSelectedPath(path);
+      setChoosedFullPathList([...choosedFullPathList, path]);
+    }
+  };
+
+  const deleteSelctedSourceCode = (path: string) => {
+    const newChoosedFullPathList = choosedFullPathList.filter(
+      el => el !== path
+    );
+    setChoosedFullPathList(newChoosedFullPathList);
+    const lastLength = newChoosedFullPathList.length;
+    const shouldDisplaySourceCodePath = newChoosedFullPathList[lastLength - 1];
+    if (shouldDisplaySourceCodePath !== undefined) {
+      changeDisplaySourceCode(shouldDisplaySourceCodePath);
+    } else {
+      setDecodedContent('');
+    }
+  };
+
+  const changeDisplaySourceCode = (path: string) => {
+    const key = getKeyForTree();
+    const encodeContent = treeObject[key].contents[path];
+    if (encodeContent === undefined) return;
+    setCurrentSelectedPath(path);
+    decodeContent(path, encodeContent);
+  };
+
+  const applySourceCode = () => {
+    const isValid = validSelectedRepositoryAndBranch();
+    if (!isValid) return;
+    const isValidChoosedFullPath = validChoosedFullPath();
+    if (!isValidChoosedFullPath) return;
+    if (isFetchContent) return;
+    const key = getKeyForTree();
+    insertToInputFileNameLists(choosedFullPathList, treeObject[key].contents);
   };
 
   return {
     choosedRepository,
-    choosedBranch,
+    choosedFullPathList,
     isChoosedRepository,
     isChoosedBranch,
-    isBranchFetch,
-    isTreeFetch,
+    isChoosedFullPath,
+    isFetchBranch,
+    isFetchTree,
+    isFetchContent,
     treeObject,
     decodedContent,
+    currentSelectedPath,
     getBranchesBySelectedRepo,
     getChoosedRepositoryBranches,
     selectBranch,
     getTree,
     getContent,
+    getKeyForTree,
+    changeDisplaySourceCode,
+    deleteSelctedSourceCode,
+    applySourceCode,
   };
 };
